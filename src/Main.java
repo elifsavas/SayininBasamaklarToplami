62ad3c8485b9d07caaa3ff4d0fd9a5665883617b
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-03 00:17:14 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k ;
+
+
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.print("Sayı Giriniz: ");
+        k = input.nextInt();
+
+        int sumNumber = 0;
+        int basNumber = 0;
+
+        while (k != 0) {
+            sumNumber += k % 10;
+
+            k /=10 ;
+
+        }
+
+       System.out.println("Basamakları Toplamı: " + sumNumber);
+
+
     }
 }
